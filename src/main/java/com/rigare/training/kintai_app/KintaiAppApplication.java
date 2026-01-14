6cbcc02b013ba09869e1fec8cc66caae8fa78470
turned025954 <sigotos657@gmail.com>
2026-01-15 00:35:08 +0900

    First commit

---
+++ src/main/java/com/rigare/training/kintai_app/KintaiAppApplication.java
@@ -0,0 +1,13 @@
+package com.rigare.training.kintai_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KintaiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KintaiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rigare/training/kintai_app/KintaiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rigare.training.kintai_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KintaiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
